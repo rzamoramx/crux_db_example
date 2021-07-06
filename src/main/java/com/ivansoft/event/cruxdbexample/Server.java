a51fff938a86c1b8856615a32b85821971a25f4e
Rodrigo Zamora <ivzamhdz@gmail.com>
2021-07-05 22:37:47 -0500

    init commit

---
+++ src/main/java/com/ivansoft/event/cruxdbexample/Server.java
@@ -0,0 +1,4 @@
+package com.ivansoft.event.cruxdbexample;
+
+public class server {
+}
---
+++ src/main/java/com/ivansoft/event/cruxdbexample/main.java
@@ -0,0 +1,7 @@
+package com.ivansoft.event.cruxdbexample;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
